0ae4a405ae4cbc77d0867f83babf037a5a198c18
Tk-Glitch <ti3nou@gmail.com>
2019-03-26 20:48:33 +0100

    proton-tkg: Add info to user_settings.py

--- proton-tkg/proton_template/conf/user_settings.py
+++ proton-tkg/proton_template/conf/user_settings.py
@@ -10,7 +10,7 @@ user_settings = {
     #Disable winedbg
      "PROTON_WINEDBG_DISABLE": "1",
 
-    #Enable IMAGE_FILE_LARGE_ADDRESS_AWARE override
+    #Enable IMAGE_FILE_LARGE_ADDRESS_AWARE override - Required by some 32-bit games hitting address space issues
 #     "PROTON_FORCE_LARGE_ADDRESS_AWARE": "1",
 
     #Reduce Pulse Latency
@@ -25,10 +25,10 @@ user_settings = {
     #Enable DXVK's HUD
 #     "DXVK_HUD": "devinfo,fps",
 
-    #Use gl-based wined3d for d3d11 and d3d10 instead of vulkan-based dxvk
+    #Use gl-based wined3d for d3d11 and d3d10 instead of vulkan-based dxvk !!! Won't affect DXVK winelib builds !!!
 #     "PROTON_USE_WINED3D": "1",
 
-    #Disable d3d11 entirely
+    #Disable d3d11 entirely !!! Won't affect DXVK winelib builds !!!
 #     "PROTON_NO_D3D11": "1",
 
     #Disable in-process synchronization primitives
