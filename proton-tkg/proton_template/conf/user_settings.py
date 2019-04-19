18e9c3340ec477ab27d28899759c7f48fb42f33c
Tk-Glitch <ti3nou@gmail.com>
2019-04-19 15:27:25 +0200

    proton-tkg: Sync with Proton 4.2-2

--- proton-tkg/proton_template/conf/user_settings.py
+++ proton-tkg/proton_template/conf/user_settings.py
@@ -1,8 +1,31 @@
 #to enable these settings, name this file "user_settings.py"
 
+#Settings here will take effect for all games run in this Proton version.
+
 user_settings = {
-    #logs are saved to $HOME/steam-$STEAM_APP_ID.log, overwriting any previous log with that name
-#     "WINEDEBUG": "", #"+timestamp,+pid,+tid,+seh,+debugstr,+module",
+    #Logs are saved to $HOME/steam-<STEAM_GAME_ID>.log, overwriting any previous log with that name.
+
+    #Wine debug logging - default="+timestamp,+pid,+tid,+seh,+debugstr,+module,+mscoree"
+#     "WINEDEBUG": "",
+
+    #DXVK debug logging
+#     "DXVK_LOG_LEVEL": "info",
+
+    #wine-mono debug logging (Wine's .NET replacement)
+     "WINE_MONO_TRACE": "E:System.NotImplementedException",
+#     "MONO_LOG_LEVEL": "info",
+
+    #Set DXVK custom config path
+#     "DXVK_CONFIG_FILE": "",
+
+    #Enable DXVK Async pipecompiler
+#     "PROTON_DXVK_ASYNC": "1",
+
+    #Enable DXVK's HUD
+#     "DXVK_HUD": "devinfo,fps",
+
+    #Write the command proton sends to wine for targeted prefix (/prefix/path/launch_command) - Helpful to track bound executable
+     "PROTON_LOG_COMMAND_TO_PREFIX": "1",
 
     #Disable nvapi and nvapi64
      "PROTON_NVAPI_DISABLE": "1",
@@ -16,29 +39,14 @@ user_settings = {
     #Reduce Pulse Latency
      "PROTON_PULSE_LOWLATENCY": "1",
 
-    #Write the command proton sends to wine for targeted prefix (/prefix/path/launch_command) - Helpful to track bound executable
-     "PROTON_LOG_COMMAND_TO_PREFIX": "1",
-
-    #Set DXVK logging level
-#     "DXVK_LOG_LEVEL": "info",
-
-    #Set DXVK custom config path
-#     "DXVK_CONFIG_FILE": "",
-
-    #Enable DXVK Async pipecompiler
-#     "PROTON_DXVK_ASYNC": "1",
-
-    #Enable DXVK's HUD
-#     "DXVK_HUD": "devinfo,fps",
-
-    #Use gl-based wined3d for d3d9/d3d10/d3d11 instead of vulkan-based dxvk & d9vk !!! Won't affect winelib builds !!!
+    #Use OpenGL-based wined3d for d3d9/d3d10/d3d11 instead of vulkan-based DXVK & D9VK !!! Won't affect winelib builds !!!
 #     "PROTON_USE_WINED3D": "1",
 
-    #Use gl-based wined3d for d3d10/11 only (keeping d9vk enabled). Comment out to use vulkan-based dxvk instead.
+    #Use gl-based wined3d for d3d10/11 only (keeping D9VK enabled). Comment out to use vulkan-based DXVK instead.
     #!!! DXVK winelib replaces wined3d d3d10/d3d11 and needs this option enabled !!!
 #     "PROTON_USE_WINED3D11": "1",
 
-    #Use gl-based wined3d for d3d9 only (keeping dxvk enabled). Comment out to use vulkan-based d9vk instead.
+    #Use gl-based wined3d for d3d9 only (keeping DXVK enabled). Comment out to use vulkan-based D9VK instead.
     #!!! D9VK winelib replaces wined3d d3d9 and needs this option enabled !!!
      "PROTON_USE_WINED3D9": "1",
 
